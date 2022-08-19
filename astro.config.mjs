342ca0fc88bdcdc192929db2b32a6ab3cdafd9e8
Jamin <jaminwebdev@gmail.com>
2022-08-18 19:59:03 -0700

    react & tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
+import image from '@astrojs/image';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [tailwind(), vue(), image()],
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    light: '#66adff',
+                    medium: '#3172bc',
+                    dark: '#0c478c',
+                },
+                secondary: '#28c4ac',
+                'body-text': {
+                    dark: '#2f495e',
+                    light: '#fafaff',
+                },
+                'body-color': {
+                    light: '#fafaff',
+                    'light-secondary': '#edf2f7',
+                    dark: '#2B4255',
+                    'dark-secondary': '#334E66',
+                },
+            },
+        },
+    },
+    plugins: [
+        plugin(function ({ addUtilities }) {
+            addUtilities({
+                '.perspective-400': {
+                    perspective: '400rem',
+                },
+                '.rotate-y-65': {
+                    transform: 'rotateY(65deg)',
+                },
+                '.origin-left-center': {
+                    'transform-origin': 'left center',
+                },
+            });
+        }),
+    ],
+};
